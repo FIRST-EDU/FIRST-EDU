41d5bfcf86c75964c4e275528eb64f796a0574cd
tj <tj@DESKTOP-MOB8F55>
2021-05-25 18:47:32 +0900

    매퍼.xml 수정 완료

--- FirstEdu/src/main/java/com/admin/firstedu/attendance/model/dao/AttendanceMapper.java
+++ FirstEdu/src/main/java/com/admin/firstedu/attendance/model/dao/AttendanceMapper.java
@@ -1,6 +1,11 @@
 package com.admin.firstedu.attendance.model.dao;
 
+import com.admin.firstedu.attendance.model.dto.AttendanceDTO;
+
 public interface AttendanceMapper {
+	
+	int insertAttendance(AttendanceDTO attendance);
+	
 	
 	 
 	
--- FirstEdu/src/main/java/com/admin/firstedu/attendance/model/dao/CategoryMapper.java
+++
@@ -1,5 +0,0 @@
-package com.admin.firstedu.attendance.model.dao;
-
-public class CategoryMapper {
-
-}
